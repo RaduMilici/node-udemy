c1ce4ff1ca00f489553bf5979c388a114f69ab83
Radu Milici <moguay@gmail.com>
2016-09-02 18:41:27 +0000

    buffers, callbacks, fs, streams

---
+++ 51_streams/51_streams.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var readable = fs.createReadStream(__dirname + '/greet.txt', 
+  { //options object
+    encoding: 'utf8', 
+    highWaterMark: 1024 // = 1 kylobyte (how big we want each chunk to be in bytes)
+  });
+  
+var writable = fs.createWriteStream(__dirname + '/greetCopy.txt');
+
+readable.on('data', function(chunk){
+  writable.write('\n------1024 bytes of text data------\n' + chunk); // will write to 'greetCopy.txt' every 1kb of read data
+});
